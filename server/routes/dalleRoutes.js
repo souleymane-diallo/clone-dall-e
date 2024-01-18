57695caf9848e25bce3822911f50b70f0f3a3673
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2024-01-18 02:14:55 +0100

    feat: clone dalle to lear mern full-stacl dev

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import  OpenAI  from "openai";
+// import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+// const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: 'Hello from DALL-E!' });
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+    console.log('aiResponse', aiResponse);
+    const image = aiResponse.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+export default router;
